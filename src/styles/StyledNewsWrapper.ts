076f19f770ce86929bdcdf34fe51e49b5e0d02ae
BohdanViershynin <viershynin.b@gmail.com>
2022-08-16 17:13:13 +0300

    pagination and responsive design added

--- src/styles/StyledNewsWrapper.ts
+++ src/styles/StyledNewsWrapper.ts
@@ -1,11 +1,16 @@
 import styled from "styled-components";
 
 const NewsWrapper = styled.div`
-  margin: 0 15px 15px 15px;
+  margin: 0 175px 15px 175px;
+
   #flexible {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+    flex-grow: 1;
+  }
+  @media screen and (max-width: 1000px) {
+    margin: 0 15px 15px 15px;
   }
 `;
 

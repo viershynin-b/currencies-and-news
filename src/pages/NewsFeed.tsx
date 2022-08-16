9bdf05db8506e125e2a271d641944f297a69553b
viershynin-b <111240290+viershynin-b@users.noreply.github.com>
2022-08-16 18:57:21 +0300

    prod-release-16-08
    pagination and responsive design added (#2)

--- src/pages/NewsFeed.tsx
+++ src/pages/NewsFeed.tsx
@@ -1,11 +1,17 @@
 import NewsFeedMain from "../components/news/NewsFeedMain";
+import * as Types from "../models/Types";
+import { useAppSelector } from "../store/hooks";
+import { RootState } from "../store";
 
 const NewsFeed = () => {
-  return (
-    <>
-      <NewsFeedMain />
-    </>
-  );
+  const fetchedNews = useAppSelector((state: RootState) => state.news.newsList);
+  let localNews: Types.INewsPosts[];
+  const response = localStorage.getItem("news");
+  response !== null
+    ? (localNews = JSON.parse(response))
+    : (localNews = fetchedNews);
+
+  return <NewsFeedMain postList={localNews} />;
 };
 
 export default NewsFeed;
